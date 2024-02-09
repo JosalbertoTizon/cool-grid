462974bd69047a0793570a66ddbe584e067da477
José Tizon <jose.tizon.9009@ga.ita.br>
2024-02-09 04:25:53 -0300

    Make user able to choose grid size

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 const container = document.querySelector(".container");
-for(let i = 0; i < 16*16; ++ i) {
-  container.appendChild(document.createElement("div"));
+
+const buildGrid = tileNumber => {
+  while(container.firstElementChild)
+    container.removeChild(container.firstElementChild)
+  for(let i = 0; i < tileNumber * tileNumber; ++ i) {
+    container.appendChild(document.createElement("div"));
+  }
+  Array.from(container.children).forEach(element => {
+    element.classList.add("pixel");
+    element.style.width = String(100/tileNumber) + "%";
+    element.addEventListener("mouseover", e => {
+      element.style.backgroundColor = "blue";
+    })
+  })
+
 }
 
-Array.from(container.children).forEach(element => {
-  element.classList.add("pixel");
-  element.addEventListener("mouseover", e => {
-    element.style.backgroundColor = "blue";
-  })
+buildGrid(16);
+
+const NumberOfTilesButton = document.querySelector(".number-of-tiles-button");
+NumberOfTilesButton.addEventListener("click", e => {
+  buildGrid(prompt("How many tiles do you want on each direction?"));
 })
